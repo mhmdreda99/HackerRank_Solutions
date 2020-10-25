41afe898423d9778844b46c71a99d947f150443a
Muhamed Reda <49542070+mhmdreda99@users.noreply.github.com>
2020-10-25 23:24:03 +0200

    Update Lists.py

--- Python Problems/Datatypes/Lists.py
+++ Python Problems/Datatypes/Lists.py
@@ -3,7 +3,8 @@ if __name__ == '__main__':
     l = []
     
     for i in range (N):
-        s = input().split()
+    #recive input    
+    s = input().split()
         
         if s[0] == "insert":
             l.insert(int(s[1]), int(s[2]))
@@ -18,4 +19,5 @@ if __name__ == '__main__':
         elif s[0] == "pop":
             l.pop()
         elif s[0] == "reverse":
+         
             l.reverse()
