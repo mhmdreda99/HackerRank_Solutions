e52aa124596018cd677dd31f7a856d861086613b
mhmdreda99 <Moreda491999@gmail.com>
2020-11-20 00:18:55 +0200

    students maarks

--- C/Functions/Students Marks Sum.c
+++ C/Functions/Students Marks Sum.c
@@ -1,14 +1,17 @@
 /**
  * @file Students Marks Sum.c
  * @author Mohmaed Reda (mhmdreda99@github.com)
- * @brief 
+ * @brief You are given an array of integers,marks , denoting the marks scored by students in a class.
+The alternating elements , marks0 ,marks2 ,marks 4  and so on denote the marks of boys.
+Similarly,marks1 ,marks3 ,marks 5  and so on denote the marks of girls.
+The array name,marks , works as a pointer which stores the base address of that array. In other words,  contains the address where  is stored in the memory.
+For example, let  and  stores 0x7fff9575c05f. Then, 0x7fff9575c05f is the memory address of
  * @version 0.1
  * @date 2020-11-19
  * 
  * @copyright Copyright (c) 2020
  * 
  */
-
 
 #include <stdio.h>
 #include <string.h>
@@ -17,11 +20,13 @@
 
 /**
  * @brief 
- * 
- * @param marks 
- * @param number_of_students 
- * @param gender 
- * @return int 
+ * the function, marks_summation(int* marks, char gender, int number_of_students) which returns the total sum of:
+  marks of boys if gendre=b
+  marks of girls if gendre=g
+ * @param marks   works as a pointer which stores the base address of that array
+ * @param number_of_students hold the number of students and contains the address where  marks is stored in the memory.
+ * @param gender hold the gender value might be g(girls) or b (boys)
+ * @return int  the output should contain the sum of all the aternate elements in marks 
  */
 int marks_summation(int* marks, int number_of_students, char gender)
 {
