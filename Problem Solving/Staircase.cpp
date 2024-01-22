d2c03c6a23d40658199002863a1ef90eea11276d
mhmdreda99 <Moreda491999@gmail.com>
2024-01-22 21:06:48 +0200

    CI

---
+++ Problem Solving/Staircase.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+#include<string>
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+
+/*
+ * Complete the 'staircase' function below.
+ *
+ * The function accepts INTEGER n as parameter.
+ */
+
+void staircase(int n) {
+    for(size_t i =1 ;i <= n;i++ ){
+        cout<< string(n -i  , ' ') << string(i , '#') << "\n" ;
+    }
+
+}
+
+int main()
+{
+    string n_temp;
+    getline(cin, n_temp);
+
+    int n = stoi(ltrim(rtrim(n_temp)));
+
+    staircase(n);
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
---
+++ Problem Solving/mini-max-sum.cpp
@@ -0,0 +1,93 @@
+#include <bits/stdc++.h>
+#include <algorithm>
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+/*
+ * Complete the 'miniMaxSum' function below.
+ *
+ * The function accepts INTEGER_ARRAY arr as parameter.
+ */
+
+void miniMaxSum(vector<int> arr) {
+    // init to vars to store the sum values 
+    int minSum = 0 ;
+    int maxSum = 0 ;
+        // sort the array
+        sort(arr.begin() , arr.end()) ; 
+    // loop through the array
+    for (int i = 0 ; i<arr.size()-1 ; i++){
+        minSum += arr[i] ;
+        maxSum += arr[arr.size()-1 -i];
+    } 
+        cout <<  minSum << " " << maxSum  << "\n"; 
+
+    }
+
+
+
+}
+
+int main()
+{
+
+    string arr_temp_temp;
+    getline(cin, arr_temp_temp);
+
+    vector<string> arr_temp = split(rtrim(arr_temp_temp));
+
+    vector<int> arr(5);
+
+    for (int i = 0; i < 5; i++) {
+        int arr_item = stoi(arr_temp[i]);
+
+        arr[i] = arr_item;
+    }
+
+    miniMaxSum(arr);
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
+
+vector<string> split(const string &str) {
+    vector<string> tokens;
+
+    string::size_type start = 0;
+    string::size_type end = 0;
+
+    while ((end = str.find(" ", start)) != string::npos) {
+        tokens.push_back(str.substr(start, end - start));
+
+        start = end + 1;
+    }
+
+    tokens.push_back(str.substr(start));
+
+    return tokens;
+}
+
